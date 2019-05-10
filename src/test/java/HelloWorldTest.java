15c67103318c303dfaf811483f99de9ada858cfe
Gregor Riegler <gregor.riegler@ebcont.com>
2019-05-10 10:31:39 +0200

    GREEN. landing Robot Receives Coordinates on landing And reports The Position.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,6 @@
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
+
+import java.util.Objects;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -13,7 +13,7 @@ public class HelloWorldTest {
 
 
         Position initialPosition = new Position(0, 0);
-        Robot robot = new Robot();
+        Robot robot = new Robot(reportingModule);
         robot.land();
 
         verify(reportingModule).reportPosition(initialPosition);
@@ -22,7 +22,15 @@ public class HelloWorldTest {
 
     public static class Robot {
 
+        private final ReportingModule reportingModule;
+
+        public Robot(ReportingModule reportingModule) {
+            this.reportingModule = reportingModule;
+        }
+
         public void land() {
+            Position currentPosition = new Position(0, 0);
+            reportingModule.reportPosition(currentPosition);
         }
     }
 
@@ -33,6 +41,23 @@ public class HelloWorldTest {
         public Position(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Position position = (Position) o;
+            return x == position.x && y == position.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
         }
     }
 }
